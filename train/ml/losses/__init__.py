148fb29596b61c583f32f9b4852d177dcd4c6786
Masahiro Noma <macharo.0316@gmail.com>
2022-12-03 20:33:37 +0000

    [loss] まとめた

---
+++ train/ml/losses/__init__.py
@@ -0,0 +1,4 @@
+from . import wave
+from .import wind
+
+__all__ = ["wave", "wind"]
---
+++ train/ml/losses/wind.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+
+class WindDirectionLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.__lossfunc = torch.nn.CrossEntropyLoss()
+
+    def forward(
+        self, p: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        t = t.to(torch.long)
+        loss = self.__lossfunc(p[:, 0:17], t[:, 0])
+        loss += self.__lossfunc(p[:, 17:34], t[:, 1])
+        loss += self.__lossfunc(p[:, 34:51], t[:, 2])
+        return loss
+
+
+class WindVelocityLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.__lossfunc = torch.nn.MSELoss()
+
+    def forward(
+        self, p: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        t = t.to(torch.float32)
+        loss = self.__lossfunc(p, t)
+        return loss
+
+
+class WindVelocityClassLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.__lossfunc = torch.nn.CrossEntropyLoss()
+
+    def forward(
+        self, p: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        t = t.to(torch.long)
+        loss = self.__lossfunc(p[:, 0:20], t[:, 0])
+        loss += self.__lossfunc(p[:, 20:40], t[:, 1])
+        loss += self.__lossfunc(p[:, 40:60], t[:, 2])
+        return loss
